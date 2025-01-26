6e7a31b74c370879abce1c2bf3316277b1eab702
NamPE286 <nambuihung654@gmail.com>
2025-01-26 20:33:33 +0700

    fix: correct column order in levels table seed data and fix user name typo

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -22,10 +22,10 @@ SET row_security = off;
 -- Data for Name: levels; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO "public"."levels" ("id", "created_at", "name", "creator_alt", "youtube_video_id") VALUES
-	(71025973, '2025-01-18 10:25:33.965152+00', 'Oblivion', 'dice88 & more', 'bsWqS5QPhz8'),
-	(52374843, '2025-01-19 18:16:01.572288+00', 'Zodiac', 'Bianox and more', 'FX9paD5rRsM'),
-	(79484035, '2025-01-26 11:17:35.230536+00', 'The Moon Below', 'Onvil', '4JA0NXdo4Wc');
+INSERT INTO "public"."levels" ("id", "created_at", "name", "youtube_video_id", "creator_alt") VALUES
+	(71025973, '2025-01-18 10:25:33.965152+00', 'Oblivion', 'bsWqS5QPhz8', 'dice88 & more'),
+	(52374843, '2025-01-19 18:16:01.572288+00', 'Zodiac', 'FX9paD5rRsM', 'Bianox and more'),
+	(79484035, '2025-01-26 11:17:35.230536+00', 'The Moon Below', '4JA0NXdo4Wc', 'Onvil');
 
 
 --
@@ -46,7 +46,7 @@ INSERT INTO "public"."user_role" ("name", "add_level", "modify_level", "delete_l
 INSERT INTO "public"."users" ("user_id", "created_at", "name", "city", "province", "role", "is_hidden") VALUES
 	('ded6b269-a856-4a49-a1ae-d8837d50e350', '2025-01-18 09:56:19.965229+00', 'default', 'Hạ Long', 'Quảng Ninh', 'default', false),
 	('fa49b543-083c-4577-958f-ca86a8e295bd', '2025-01-24 04:02:28.566346+00', 'default1', NULL, NULL, 'default', false),
-	('570eef3f-5510-4b72-94f7-b5d325a70d11', '2025-01-26 11:19:35.451648+00', 'Onvli', NULL, NULL, 'default', false);
+	('570eef3f-5510-4b72-94f7-b5d325a70d11', '2025-01-26 11:19:35.451648+00', 'Onvil', NULL, NULL, 'default', false);
 
 
 --
